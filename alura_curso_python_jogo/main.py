a6917a787b7e0b359e45f843f6fdcf0f49e089d5
WellingtonLPuerta <wellington.mesa.puerta@gmail.com>
2022-07-28 06:19:48 -0300

    Studynig py  with Alura

---
+++ alura_curso_python_jogo/main.py
@@ -0,0 +1,18 @@
+print("#################################")
+print("Welcome to the of divination!")
+print("#################################")
+
+n_secret = 57
+
+# a função input permite que o usuário digite o input. 
+# obs: o código fica ligado até o valor ser inserido, depois de inserido
+# o código segue a compilação
+
+n_kick = int(input("Enter the number:"))
+
+print("The number sent is:",n_kick)
+
+if (n_secret == n_kick):
+    print("You win !")
+else:
+    print("You loser !")    

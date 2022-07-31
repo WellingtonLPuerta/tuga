5536910907584b132b47d8503f9c1a65a361142b
WellingtonLPuerta <wellington.mesa.puerta@gmail.com>
2022-07-31 06:02:56 -0300

    increment nivel game

--- alura_curso_python_jogo/main.py
+++ alura_curso_python_jogo/main.py
@@ -6,40 +6,54 @@ print("#################################")
 
 #n_secret = 57
 
-n_secret = random.randrange(1,101)
-#print(n_secret)
+n_secret = random.randrange(1, 101)
+print(n_secret)
 ###################################
 # n try
 ###################################
 n_try = 3
 #game = 1
+total_n_try = 0
 
-# a função input permite que o usuário digite o input.
-# obs: o código fica ligado até o valor ser inserido, depois de inserido
-# o código segue a compilação
+
+###################################
+# select nivel game
+###################################
+
+print("Choose the difficulty level")
+print("(1) easy (2) medium (3) hard")
+nivel_game = int(input("Input nivel game:"))
+
+
+if (nivel_game == 1):
+    total_n_try = 20
+elif (nivel_game == 2):
+    total_n_try = 10
+else:
+    total_n_try = 5
 
 ###################################
 # while
 ###################################
-for game  in range(1,n_try+1):
-    print("The number the tray is:", n_try)
 
-    print("Game {} of {}.".format(game,n_try))
+for game in range(1, total_n_try+1):
+    print("The number the tray is:", total_n_try)
+
+    print("Game {} of {}.".format(game, total_n_try))
 ###################################
 # kick
 ###################################
 
     n_kick = int(input("Enter the number (input between 1 and 100):"))
 
-    if (n_kick < 1 or n_kick >100):
+    if (n_kick < 1 or n_kick > 100):
         print("The number have to be between 1 and 100 !")
-    continue
+        continue
 
 ###################################
 # isolating the conditions
 ###################################
-
-    hit   = (n_secret == n_kick)
+    hit = (n_secret == n_kick)
     above = (n_kick > n_secret)
     below = (n_kick < n_secret)
 
@@ -50,12 +64,12 @@ for game  in range(1,n_try+1):
 
     if (hit):
         print("You win !")
-        break #sai do laço    
+        break  # sai do laço
     else:
         if (above):
             print("Your kick is above the secret value")
         elif (below):
-            print("Your kick is below the secret value")      
-    
+            print("Your kick is below the secret value")
+
  #   game = game + 1
 print("The end game.")

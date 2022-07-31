57a2b95f1540c7ade4a727358113467af3b97bd0
WellingtonLPuerta <wellington.mesa.puerta@gmail.com>
2022-07-31 06:28:06 -0300

    alter name adivinhacao and select game

---
+++ alura_curso_python_jogo/select_games.py
@@ -0,0 +1,14 @@
+import forca
+import main
+
+print("#################################")
+print("Welcome to the Games !")
+print("#################################")
+
+print("Choose your game end play")
+game_select = int(input("(1)Adivinhacao (2)Foca."))
+
+if (game_select == 1):
+    print("Select Adivinhacao.")
+elif (game_select == 2):
+    print("Select Forca.") 

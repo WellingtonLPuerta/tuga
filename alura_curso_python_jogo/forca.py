490ac9d4072fa15efa52e2e053da858498d7f295
WellingtonLPuerta <wellington.mesa.puerta@gmail.com>
2022-08-03 06:38:20 -0300

    dev hangman

--- alura_curso_python_jogo/forca.py
+++ alura_curso_python_jogo/forca.py
@@ -1,10 +1,28 @@
 
 def game():
     print("#################################")
-    print("Welcome to the of forca!")
+    print("Welcome to the of hangman game!")
     print("#################################")
 
+    word_secret = "banana"
+
+    hanged = False
+    hit = False
+
+    while (not hanged and not hit):
+        letter_kick = input("Report the letter:")
+        #word_secret.find(letter_kick)
+        for word_list in word_secret:
+            if(letter_kick == word_list):
+                print(letter_kick)
+
+
+        print("Playing")
+
+
+
+
     print("End game")
-    
+
 if (__name__ == "__main__"):
     game()   

06b87df0923d1ea83f2fb238a4dc0b85b6525845
WellingtonLPuerta <wellington.mesa.puerta@gmail.com>
2022-08-03 07:00:01 -0300

    increment position

--- alura_curso_python_jogo/forca.py
+++ alura_curso_python_jogo/forca.py
@@ -10,13 +10,16 @@ def game():
     hit = False
 
     while (not hanged and not hit):
+
         letter_kick = input("Report the letter:")
         #word_secret.find(letter_kick)
+
+        index = 0 
         for word_list in word_secret:
             if(letter_kick == word_list):
-                print(letter_kick)
-
-
+                print("Found word {} in position {}".format(letter_kick,index))
+            index = index + 1
+            
         print("Playing")
 
 
@@ -24,5 +27,7 @@ def game():
 
     print("End game")
 
+# Usa-se o __name__ para proporcionar o código a possibilodade
+# de ser executado direto ou via "import" em outros códigos.
 if (__name__ == "__main__"):
     game()   

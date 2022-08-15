e2a2b86e88096c3bcf7d90c0d1e96261c1b9bf37
WellingtonLPuerta <wellington.mesa.puerta@gmail.com>
2022-08-15 09:11:09 -0300

    introduce the fucntion structure

--- mauri_academy/triangle_validator.py
+++ mauri_academy/triangle_validator.py
@@ -1,27 +1,33 @@
-print("###################################")
-print("######## Triangle validator #######")
-print("###################################")
+def game():
 
-array = [0,0,0]
 
-for i in range(0,len(array )):
-    array[i] = input("Write the {}º value:".format(i))
+    print("###################################")
+    print("######## Triangle validator #######")
+    print("###################################")
 
-print("The vector write is {}".format(array))
+    array = [0,0,0]
 
-### inside the text if it's a valid triangle.
-win = 'The values write stisfy the problem it. Its a triangle !'
-lose = 'The values weite not stisfy the problem it.'
-    
-if array[0]<(array[1]+array[2]):
+    for i in range(0,len(array )):
+        array[i] = input("Write the {}º value:".format(i))
+
+    print("The vector write is {}".format(array))
+
+    ### inside the text if it's a valid triangle.
+    win = 'The values write stisfy the problem it. Its a triangle !'
+    lose = 'The values weite not stisfy the problem it.'
+        
     if array[0]<(array[1]+array[2]):
         if array[0]<(array[1]+array[2]):
-            print(win)
+            if array[0]<(array[1]+array[2]):
+                print(win)
+            else:
+                print(lose)
         else:
             print(lose)
     else:
         print(lose)
-else:
-    print(lose)
-    
-                
+if (__name__ == "__main__"):
+    game()   
+
+        
+                    
